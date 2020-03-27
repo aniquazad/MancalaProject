1f823ac22774f10e00f6bdb6be7debebc663c2e9
aniquazad <aniqua.azad@gmail.com>
2020-03-26 23:36:11 -0700

    Add files via upload

---
+++ MancalaGameTester.java
@@ -0,0 +1,11 @@
+package ram;
+
+public class MancalaGameTester 
+{
+	public static void main(String[] args)
+	{
+		MancalaGameModel m = new MancalaGameModel();
+		//MancalaGameView v = new MancalaGameView(m);
+		//m.attach(v);
+	}
+}
