0453d3e033d1c9b15c78831e69dc92c1fae0e86f
ryan_tran <ryantran2165@gmail.com>
2020-04-25 10:19:12 -0700

    Rename MancalaTester to MancalaTest per project spec

---
+++ MancalaTest.java
@@ -0,0 +1,12 @@
+package ram;
+
+/**
+ * Calls MenuView to run the application.
+ *
+ * @author Aniqua Azad, Malaak Khalil, Ryan Tran
+ */
+public class MancalaTest {
+  public static void main(String[] args) {
+    new MenuView();
+  }
+}
